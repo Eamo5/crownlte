d82276389b89602b96f48c18a9237321353376ef
Eamo5 <eamonpowell@hotmail.com>
2019-01-14 11:12:38 +1100

    Revert "net: let knox be disabled"
    This reverts commit d2261d1c0d304a9c932722ecee541243e41a79f1.

--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -56,11 +56,9 @@
 #include <net/netfilter/nf_nat_helper.h>
 #include <net/netns/hash.h>
 
-#ifdef CONFIG_KNOX_NCM
 /* START_OF_KNOX_NPA */
 #include <net/ncm.h>
 /* END_OF_KNOX_NPA */
-#endif
 
 #define NF_CONNTRACK_VERSION	"0.5.0"
 
@@ -323,14 +321,12 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 {
 	struct ct_pcpu *pcpu;
 
-	#ifdef CONFIG_KNOX_NCM
 	/* START_OF_KNOX_NPA */
 	/* send dying conntrack entry to collect data */
 	if ( (check_ncm_flag()) && (ct != NULL) && (atomic_read(&ct->startFlow)) ) {
 		knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_CLOSE, 10);
 	}
 	/* END_OF_KNOX_NPA */
-	#endif
 
 	/* add this conntrack to the (per cpu) dying list */
 	ct->cpu = smp_processor_id();
@@ -1059,11 +1055,9 @@ __nf_conntrack_alloc(struct net *net,
 		     gfp_t gfp, u32 hash)
 {
 	struct nf_conn *ct;
-	#ifdef CONFIG_KNOX_NCM
 	/* START_OF_KNOX_NPA */
 	struct timespec open_timespec;
 	/* END_OF_KNOX_NPA */
-	#endif
 
 	/* We don't want any race condition at early drop stage */
 	atomic_inc(&net->ct.count);
@@ -1086,7 +1080,6 @@ __nf_conntrack_alloc(struct net *net,
 		goto out;
 
 	spin_lock_init(&ct->lock);
-	#ifdef CONFIG_KNOX_NCM
 	/* START_OF_KNOX_NPA */
 	/* initialize the conntrack structure members when memory is allocated */
 	if (ct != NULL) {
@@ -1105,7 +1098,6 @@ __nf_conntrack_alloc(struct net *net,
 		atomic_set(&ct->startFlow, 0);
 	}
 	/* END_OF_KNOX_NPA */
-	#endif
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple = *orig;
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].hnnode.pprev = NULL;
 	ct->tuplehash[IP_CT_DIR_REPLY].tuple = *repl;
