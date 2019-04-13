ca1fe52e3cffc94de0369e3037f304db87f00c90
Eamo5 <eamonpowell@hotmail.com>
2019-04-13 14:40:54 +1000

    ion: Fix merge error

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -368,7 +368,7 @@ static int ion_system_heap_create_pools(struct ion_page_pool **pools,
 		gfp_t gfp_flags = low_order_gfp_flags;
 
 		if (orders[i] < 4)
-			gfp_flags = low_order_gfp_flags;
+			gfp_flags = high_order_gfp_flags;
 
 		pool = ion_page_pool_create(gfp_flags, orders[i], cached);
 		if (!pool)
